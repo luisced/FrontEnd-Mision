dc5d576df83a9fcde7ebd3132cd37ce8ab0eb602
Luis <luisced02031@gmail.com>
2022-03-29 18:14:19 -0600

    vue practice finished

--- Ejercicios/VUE_js/vuex/src/views/HomeView.vue
+++ Ejercicios/VUE_js/vuex/src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+
+    <Contador/>
+    <Cuadrado/>
+    <Botones/>
+
+
   </div>
+    
+
+
+
 </template>
 
 <script>
+import Contador from '../components/Contador.vue'
+import Cuadrado from '../components/Cuadrado.vue'
+import Botones from '../components/Botones.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+     Contador, 
+     Cuadrado,
+     Botones,
+      },
+	name:'HomeView',
   }
-}
 </script>
+
+
+<style>
+
+div{
+  margin-bottom: 10px;
+}
+   
+  button{
+    font-size: 40px;
+    font-weight: bold;
+    color: #42b983;
+    margin: 0px 10px;
+   }
+
+ .contador{
+  font-size: 50px;
+  font-weight: bold;
+  color: #42b983;
+ }
+
+.cuadrado{
+  font-size: 20px;
+  font-weight: bold;
+  color: #42b983;
+}
+
+
+</style>
